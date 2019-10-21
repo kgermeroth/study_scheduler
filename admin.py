42557c14a90f273e6a821abb94783a09cb462921
Kristin Germeroth <kristin.germeroth@gmail.com>
2019-10-21 16:30:50 -0700

    added function to add institution to database

--- admin.py
+++ admin.py
@@ -1,9 +1,11 @@
+from model import *
+
 def print_options():
 	"""Prints list of user options to the terminal"""
 
 	print('Menu of items:')
 	print('\tA: Add an institution')
-	print('\tQ: Quit')
+	print('\tQ: Quit\n')
 
 
 def choose_options():
@@ -13,7 +15,7 @@ def choose_options():
 
 	available_options = {'A', 'Q'}	
 
-	while keep_going is True:
+	while keep_going:
 
 		print_options()
 
@@ -28,12 +30,33 @@ def choose_options():
 			print('You are now leaving the program. Have a wonderful day!')
 			break
 
+		# if user chooses 'A' for 'Add an Institution'
+		elif admin_choice == 'A':
+			add_institution()
+
 
 def add_institution():
 	"""Adds an institution to the database"""
 
-	pass
+	print('\nYou will be adding a new institution to the database.\n')
+	institution_to_add = input('What is the name of the institution to add?: ')
+	print()
+
+	# double check the spelling
+	confirm = input(f'Is "{institution_to_add}" correct? Confirm Y or N: ').upper()
+
+	# if not correct start over
+	if confirm.startswith('N'):
+		print()
+		return
+
+	# if correct, add institution to the database
+	else:
+		institution = Institution(institution_name=institution_to_add)
+		db.session.add(institution)
+		db.session.commit()
+		print(f'\n"{institution_to_add}" has been successfully added.\n')
 
 if __name__ == '__main__':
-
+	init_app()
 	choose_options()
