64f27b7ea8a79a574ee1a01a3e36eb7fafeb3f7a
Kristin Germeroth <kristin.germeroth@gmail.com>
2019-10-21 15:01:31 -0700

    finished and tested functions to seed database with frequency and timezone data

--- seed.py
+++ seed.py
@@ -1,2 +1,55 @@
+"""Utility file to seed hotels database from collected data"""
+
 from sqlalchemy import func
-from model import connect_to_db, db, init_app, Timezones, Frequency
+from model import connect_to_db, db, init_app, Timezone, Frequency
+
+def load_frequencies():
+	"""Loads frequncies from text file into database."""
+
+	# open file where data is stored
+	file = open('seed_files/frequency.txt')
+
+	# loop through each line and add it to the database
+	for line in file:
+		line = line.rstrip()
+
+		# instantiate a Frequncy object and add it to database
+		frequency = Frequency(frequency=line)
+
+		db.session.add(frequency)
+
+	db.session.commit()
+
+	file.close()
+
+
+def load_timezones():
+	"""Loads timezones from text file into database."""
+
+	# open file where data is stored
+	file = open('seed_files/timezones.txt')
+
+	# loop through each line and add it to the database
+	for line in file:
+		line = line.rstrip()
+
+		# instantiate a Timezone object and add it to the database
+		timezone = Timezone(timezone_name=line)
+
+		db.session.add(timezone)
+
+	db.session.commit()
+
+	file.close()
+
+
+if __name__ == "__main__":
+	init_app()
+
+	# delete all rows in table in case need to import multiple times
+	
+	Frequency.query.delete()
+	Timezone.query.delete()
+
+	load_frequencies()
+	load_timezones()
