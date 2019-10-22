36b8f0f045610027b3bb7e349259e5dc4173488e
Kristin Germeroth <kristin.germeroth@gmail.com>
2019-10-21 17:14:20 -0700

    added function to handle adding department to database

--- admin.py
+++ admin.py
@@ -5,6 +5,7 @@ def print_options():
 
 	print('Menu of items:')
 	print('\tA: Add an institution')
+	print('\tB: Add a department to an institution')
 	print('\tQ: Quit\n')
 
 
@@ -13,7 +14,7 @@ def choose_options():
 	
 	keep_going = True
 
-	available_options = {'A', 'Q'}	
+	available_options = {'A', 'B', 'Q'}	
 
 	while keep_going:
 
@@ -33,6 +34,10 @@ def choose_options():
 		# if user chooses 'A' for 'Add an Institution'
 		elif admin_choice == 'A':
 			add_institution()
+
+		# if user chooses 'B' for add a department
+		elif admin_choice == 'B':
+			add_departments()
 
 
 def add_institution():
@@ -56,6 +61,54 @@ def add_institution():
 		db.session.add(institution)
 		db.session.commit()
 		print(f'\n"{institution_to_add}" has been successfully added.\n')
+
+
+def add_departments():
+	"""Adds department to a given institution"""
+
+	instit_dict = {}
+
+	# get a list of institutions from database
+	institutions = Institution.query.all()
+
+	print('You have chosen to add a department to an institution.\n')
+
+	print('Available institutions:')
+	# print out a list of institutions and create a dictionary
+	for institution in institutions:
+		inst_id, inst_name = institution.institution_id, institution.institution_name
+		instit_dict[inst_id] = inst_name
+		print(f'{inst_id}: {inst_name}')
+
+	# have user select from list of options
+	chosen_instit = input('\nChoose the number of institution to add department to: ')
+
+	# check user input to verify valid
+	if not ((chosen_instit.isdigit()) and (int(chosen_instit) in instit_dict)):
+		print('That is not a valid selection.\n')
+		return
+
+	chosen_instit = int(chosen_instit)
+
+	# if valid, have user enter name of department
+	dept_to_add = input('\nPlease enter department name: ')
+
+	# confirm to user entry is correct
+	print(f'\nYou would like to add "{dept_to_add}" to "{instit_dict[chosen_instit]}"?')
+	confirm = input('Y or N? ').upper()
+
+	# if no, return
+	if confirm.startswith('N'):
+		print()
+		return
+
+	# if yes, add to database and confirm to user it has been updated
+	location = Location(department_name=dept_to_add, institution_id=chosen_instit)
+	db.session.add(location)
+	db.session.commit()
+
+	print(f'\n"{dept_to_add}" has been added to the database.\n')
+
 
 if __name__ == '__main__':
 	init_app()
