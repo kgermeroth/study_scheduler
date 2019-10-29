9cf084bcd63d8fe73619b6424c5b23c817e53cea
Kristin Germeroth <kristin.germeroth@gmail.com>
2019-10-29 12:58:17 -0700

    added functions to convert submitted time into military time

--- schedule_funcs.py
+++ schedule_funcs.py
@@ -13,3 +13,72 @@ def get_timeslots(project_id):
 
 	return Project_Times.query.filter(Project_Times.project_id == project_id).all()
 
+
+def parse_timeframes_from_submission(submission):
+	"""Takes submission and parses into usable format.
+
+	Returns [{'freq' : 'once', 'repeat_in' : 5, 'start_date' : datetime, 'end_date' : datetime}, {etc}]"""
+
+	parsed_timeframes = []
+
+	ids = submission.getlist('div_ids')
+
+	for d in ids:
+		timeframe_dict = {}
+
+		# pull out frequency and add to dictionary
+		freq = 'freq_' + d
+		freq = submssion[freq]
+		timeframe_dict['freq'] = freq
+
+		# pull out repetition and add to dictionary
+		rep = 'repeat_in_' + d
+		rep = submission[rep]
+		timeframe_dict['repeat_in'] = rep
+
+		# take dates and convert them into datetime objects
+		start_day = 'starting_on_' + d
+		start_day = submission[start_day]
+
+		time = 'time_' + d
+		time = submission[d]
+
+		start_time, end_time = convert_to_military_time(time)
+
+
+
+def convert_to_military_time(time):
+	"""Returns start and end time as military time
+
+	>>>'11:00 AM - 01:00 PM' 
+	(11:00, 13:00) 			"""
+
+	start_hour = int(time[:2])
+	start_minutes = time[3:5]
+	start_meridian = time[6:8]
+
+	start_time = convert_hours_military(start_hour, start_meridian) + ":" + start_minutes
+
+	end_hour = int(time[-4:-6])
+	end_minutes = time[-5:-3]
+	end_meridian = time[-2:]
+
+	end_time = convert_hours_military(end_hour, end_meridian) + ":" + end_minutes
+
+	return(start_time, end_time)
+
+
+def convert_hours_military(hour, meridian):
+	"""Takes hour and am/pm and returns the hour in military as a string"""
+
+	if hour == 12:
+		hour = 0
+
+	if meridian == 'PM':
+		hour += 12
+
+	return str(hour)
+
+
+
+
